b73b0a723461ebb22e14dbba83a6ecac2f6adb84
Manuel Geraldes <manuelgeraldes2009@hotmail.com>
2022-03-29 17:04:24 +0100

    Add comments to TakeDamage method

--- MyGame/Enemy.cs
+++ MyGame/Enemy.cs
@@ -20,14 +20,31 @@ namespace MyGame
             return name;
         }
 
+        /// <summary>
+        /// Decrements given damage value from shield value and, 
+        /// if this value is at 0, decrements from health as well.
+        /// </summary>
+        /// <param name="damage">
+        /// The value to decrement from health and shield
+        /// </param>
         public void TakeDamage(float damage)
         {
+            // Decrement damage value from shield
             shield -= damage;
+
+            // If shield value is lower than 0
             if (shield < 0)
             {
+                // Assign negative shield value to new float
                 float damageStillToInflict = -shield;
+
+                // Set shield value at 0
                 shield = 0;
+
+                // Decrement damage still to inflict from health
                 health -= damageStillToInflict;
+
+                // If health value is lower than 0, health is set at 0
                 if (health < 0) health = 0;
             }
         }
