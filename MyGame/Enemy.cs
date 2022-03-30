fb1dbbb3b6f366d1d2e96fbfe24628a7132c468b
Manuel Geraldes <manuelgeraldes2009@hotmail.com>
2022-03-30 14:51:50 +0100

    Update constructor with SetName method

--- MyGame/Enemy.cs
+++ MyGame/Enemy.cs
@@ -10,7 +10,7 @@ namespace MyGame
 
         public Enemy(string name)
         {
-            this.name = name;
+            SetName(name);
             health = 100;
             shield = 0;
         }
