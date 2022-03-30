f46406c4ef01d82ddf32aa1ff575662ccd78f675
Manuel Geraldes <manuelgeraldes2009@hotmail.com>
2022-03-30 15:35:57 +0100

    Request and assign name for each enemy

--- MyGame/Program.cs
+++ MyGame/Program.cs
@@ -20,6 +20,22 @@ namespace MyGame
 
             // Set array size as number of enemies
             enemies = new Enemy[numOfEnemies];
+
+            // Loop for each  number of enemies
+            for (int i = 0; i < numOfEnemies; i++)
+            {
+                // Name for each enemy
+                string enemyName;
+
+                // Request current enemy name
+                Console.Write($"Name for enemy {i + 1}: ");
+
+                // Store user input as name
+                enemyName = Console.ReadLine();
+
+                // Assign name for current enemy
+                enemies[i] = new Enemy(enemyName);
+            }
         }
     }
 }
